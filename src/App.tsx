fe7c107517acb568ede5932e9fe6baf8da2a800e
Raju Ahmmed <ahraju2003@gmail.com>
2025-05-28 16:06:27 +0600

    update app tx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
-
 function App() {
   return (
     <div>
-      <h1 className="text-xl text-red-500">PH University Management Application</h1>
+      <h1 className="text-4xl">
+        PH University Management Application
+      </h1>
     </div>
   );
 }
