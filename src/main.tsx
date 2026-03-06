6b993044d8a552e2a78595f377559a1ffb34e5c6
MD RAJU AHMMED <ahraju2003@gmail.com>
2026-03-06 22:42:53 +0600

    setup redex

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes.tsx";
+import { store } from "./redux/features/store.ts";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>,
 );
