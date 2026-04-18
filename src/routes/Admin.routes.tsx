0a5ccab1e31f3825641a8308352b05b4fffdfa3f
MD RAJU AHMMED <ahraju2003@gmail.com>
2026-04-18 22:09:08 +0600

    update

--- src/routes/Admin.routes.tsx
+++ src/routes/Admin.routes.tsx
@@ -65,7 +65,7 @@ export const adminSidebarItems = adminPaths.reduce<any[]>((acc, item) => {
   return acc;
 }, []);
 
-// admin route
+// admin routes for
 export const adminRoutes = adminPaths.reduce<RouteObject[]>((acc, item) => {
   if (item.path && item.element) {
     acc.push({
